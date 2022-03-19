75d02b8122165e9d5fe641c25298f12dff470931
yangj <yangjin94@163.com>
2022-03-19 22:40:24 +0800

    add build flags

--- scripts/make-rules/common.mk
+++ scripts/make-rules/common.mk
@@ -6,3 +6,11 @@ COMMON_SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 ifeq ($(origin ROOT_DIR),undefined)
 ROOT_DIR := $(abspath $(shell cd $(COMMON_SELF_DIR)/../.. && pwd -P))
 endif
+
+#
+ifeq ($(origin VERSION), undefined)
+VERSION := $(shell git describe --tags --always --match='v*')
+endif
+GIT_TREE_STATE:="dirty"
+
+GIT_COMMIT:=$(shell git rev-parse HEAD)
