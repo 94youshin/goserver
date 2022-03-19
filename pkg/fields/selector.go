9329393a7972af7ad6650433e6a2121d07afb215
yangj <yangjin94@163.com>
2022-03-20 05:18:55 +0800

    selector interface

--- pkg/fields/selector.go
+++ pkg/fields/selector.go
@@ -1,6 +1,17 @@
 package fields
 
 type Selector interface {
+	Matches(fields Fields) bool
+
+	Empty() bool
+
+	RequiresExactMatch(field string) (value string, found bool)
+
+	Transform()
+
+	String() string
+
+	DeepCopySelector() Selector
 }
 
 func SelectorFromSet(ls Set) Selector {
