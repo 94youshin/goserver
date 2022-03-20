dc6b16b989feb59eae7b16a485977d5eb3450b00
yangj <yangjin94@163.com>
2022-03-21 06:55:13 +0800

    user impl

---
+++ internal/apiserver/store/mysql/user.go
@@ -0,0 +1,43 @@
+package mysql
+
+import (
+	"context"
+	"errors"
+
+	v1 "github.com/usmhk/goserver/internal/apiserver/model/v1"
+	"github.com/usmhk/goserver/pkg/errno"
+	"gorm.io/gorm"
+)
+
+type users struct {
+	db *gorm.DB
+}
+
+func newUsers(ds *datastore) *users {
+	return &users{ds.db}
+}
+
+func (u *users) Create(ctx context.Context, user *v1.User, opts metav1.CreateOptions) error {
+	return u.db.Create(&user).Error
+}
+
+func (u *users) Update(ctx context.Context, user *v1.User, opts metav1.UpdateOptions) error {
+	return nil
+}
+
+func (u *users) Delete(ctx context.Context, username string, opts metav1.DeleteOptions) error {
+	return nil
+}
+
+func (u *users) Get(ctx context.Context, username string, opts metav1.GetOptions) (*v1.User, error) {
+	user := &v1.User{}
+	err := u.db.Where("username = ?", username).First(&user).Error
+	if err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, errno.ErrPageNotFound
+	}
+	return user, nil
+}
+
+func (u *users) List(ctx context.Context, opts metav1.ListOptions) (*v1.UserList, error) {
+	return nil, nil
+}
